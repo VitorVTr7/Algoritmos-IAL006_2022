9b35e18338da098c9e94cfdfbdee47c234918db5
VitorVTr7 <24202062@aluno.univesp.br>
2026-05-07 01:07:31 -0300

    Titulo de Exercicio

--- 06 - Exercícios/Exercicio 2 - Lista 3/main.cpp
+++ 06 - Exercícios/Exercicio 2 - Lista 3/main.cpp
@@ -1,3 +1,8 @@
+/*
+Fulano tem 1,50m e cresce 2cm por ano, enquanto Ciclano tem 1,10m e 
+cresce 3cm por ano. Faça um programa que calcule e imprima quantos anos 
+serão necessários para que Ciclano seja maior que Fulano
+*/
 #include <iostream>
 
 using namespace std;
