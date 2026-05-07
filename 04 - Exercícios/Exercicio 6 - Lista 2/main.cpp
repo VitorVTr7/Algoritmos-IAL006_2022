4d1c2a247afa9596908fc8cf8bd20440f0dced67
VitorVTr7 <24202062@aluno.univesp.br>
2026-05-07 00:44:33 -0300

    Titulo de Exercicio

--- 04 - Exercícios/Exercicio 6 - Lista 2/main.cpp
+++ 04 - Exercícios/Exercicio 6 - Lista 2/main.cpp
@@ -1,3 +1,15 @@
+/*
+Faça um programa que dado as três notas de um aluno, 
+determine e exiba a sua média final e o seu conceito, sabendo-se que:
+
+A média final é calculada pela média aritmética das 3 notas. 
+
+O conceito é determinado de com base na tabela a seguir: 
+MÉDIA FINAL 				CONCEITO 
+>= 8.0						A
+>= 5.0 e < 8.0				B
+< 5.0						C
+*/
 #include <iostream>
 
 using namespace std;
