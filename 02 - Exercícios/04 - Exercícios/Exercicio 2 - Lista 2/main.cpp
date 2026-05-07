2ceaadbec8ceffab9c1f75202aa897b2cd507fb7
VitorVTr7 <24202062@aluno.univesp.br>
2026-05-06 23:52:59 -0300

    Lista Exercício 2

---
+++ 02 - Exercícios/04 - Exercícios/Exercicio 2 - Lista 2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cout << "Coloque um numero: " << endl;
+    cin >> x;
+    cout << "Coloque outro numero: " << endl;
+    cin >> y;
+
+    if (x > y){
+        cout << "seu numero e " << x << "." << endl;
+    }else{
+        cout << "seu numero e " << y << "." << endl;
+
+    }
+    return 0;
+}
