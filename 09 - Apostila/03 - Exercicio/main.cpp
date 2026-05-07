98c71ecc7cb453488f56e51880833eaa5173ffe3
VitorVTr7 <24202062@aluno.univesp.br>
2026-05-07 05:55:49 -0300

    Titulo de Exercicio

--- 09 - Apostila/03 - Exercicio/main.cpp
+++ 09 - Apostila/03 - Exercicio/main.cpp
@@ -1,3 +1,11 @@
+/*
+Fazer um programa que possua uma rotina que leia uma entrada do usuário e faça a 
+validação da entrada de dados S/s ou N/n. 
+Só permita a saída da rotina se o usuário digitou S/s ou N/n. 
+Faça um programa para testar sua rotina. A rotina deverá ter o cabeçalho a seguir.
+
+void validaEntrada(char *ent); 
+*/
 #include <iostream>
 
 using namespace std;
